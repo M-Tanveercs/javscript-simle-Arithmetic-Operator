d70319e777647d82e7cecc58ee755600592f21f5
M-Tanveer <m.tanveercs@gmail.com>
2021-08-11 02:23:34 -0700

    arithmatic operation

---
+++ main.js
@@ -0,0 +1,21 @@
+var name="Muhammad Tanveer";
+console.log("My name is:",name);
+console.log("-------------..................--------------");
+
+var num1,num2;
+
+num1=parseInt("10");
+num2=parseInt("5");
+console.log(" 1st number is",num1);
+console.log(" 2nd number is",num2);
+
+var sum=num1+num2;
+console.log("The sum of 1st & 2nd number is: ",sum);
+var sub=num1-num2;
+console.log("The subtraction of 1st & 2nd number is: ",sub);
+var mul=num1*num2;
+console.log("The multiplication of 1st & 2nd number is: ",mul);
+var div=num1/num2;
+console.log("The division of 1st & 2nd number is: ",div);
+var mod=num1%num2;
+console.log("The modulas of 1st and 2nd number is:",mod);
